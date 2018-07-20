9ee9a8450f27b20e42a0454c5a11ea652d72313e
Kanika <kanikachander1@gmail.com>
2018-07-20 12:56:27 +0530

    Done

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,27 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
+            string rna = "";
+            foreach(char c in nucleotide)
+            {
+                string str = "";
+                switch (c)
+                {
+                    case 'G': str = "C";
+                        break;
+                    case 'C': str = "G";
+                        break;
+                    case 'T': str = "A";
+                        break;
+                    case 'A': str = "U";
+                        break;
+                }
+                rna += str;
+                
+            }
+            return rna;
             throw new NotImplementedException("You need to implement this function.");
+            
         }
     }
 }
